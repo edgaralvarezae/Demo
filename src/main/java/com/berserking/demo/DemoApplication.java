4df57304964d8d5dcb53d988c6f3a95b9f60ea65
Edgar Alvarez <edgar.alvareza@outlook.com>
2025-08-31 20:28:24 -0600

    Demo first version.

--- src/main/java/com/berserking/demo/DemoApplication.java
+++ src/main/java/com/berserking/demo/DemoApplication.java
@@ -2,12 +2,23 @@ package com.berserking.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.berserking.demo.utils.Constants;
 
+import static com.berserking.demo.utils.Constants.MENSAJE_CLIMA_OK;
+
+@RestController
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/getClima")
+	public String getMessage(){
+		return MENSAJE_CLIMA_OK;
 	}
 
 }
