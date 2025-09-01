04366b4190d8f5a50a8276f3daf2b40c317c098e
Edgar Alvarez <edgar.alvareza@outlook.com>
2025-08-31 20:29:00 -0600

    Demo first version.

--- src/main/java/com/berserking/demo/DemoApplication.java
+++ src/main/java/com/berserking/demo/DemoApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.berserking.demo.utils.Constants;
 
 import static com.berserking.demo.utils.Constants.MENSAJE_CLIMA_OK;
 
